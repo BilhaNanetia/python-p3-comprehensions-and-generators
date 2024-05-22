049002833067e3b76b57fe752782ffc1259b37c1
Bilha <bilha.leposo@student.moringaschool.com>
2024-05-22 14:32:50 +0300

    completes comprehensions lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    '''returns evens from num_list'''
+    return [num for num in num_list if num % 2 == 0]
+    
 
 def make_exclamation(sentence_list):
-    pass
+    '''returns list of sentences with exclamation marks'''
+    return [sentence + "!" for sentence in sentence_list]
+    
